3ed5bb1f2258fcdb0d18a771e5709bd1a2487995
600020344 <77069279+600020344@users.noreply.github.com>
2021-01-06 10:05:19 -0800

    Update FirstLastVowel.java

--- FirstLastVowel.java
+++ FirstLastVowel.java
@@ -8,6 +8,23 @@ public class FirstLastVowel
 {
    public static String go( String a )
  {
-  return "add code here";
+   //c is character to check. Set c to first character of a
+   String c = a.substring(0,1);
+   //check if c, when lowercase, is a vowel
+  if (c.toLowerCase().equals("a")||c.toLowerCase().equals("e")||c.toLowerCase().equals("i")||c.toLowerCase().equals("o")||c.toLowerCase().equals("u"))
+  {
+    //if so return yes
+    return "yes";
+  }
+  //set c to last character of a
+  c = a.substring(a.length()-1);
+  //check if c, when lowercase, is a vowel
+  if (c.toLowerCase().equals("a")||c.toLowerCase().equals("e")||c.toLowerCase().equals("i")||c.toLowerCase().equals("o")||c.toLowerCase().equals("u"))
+  {
+    //if so return yes
+    return "yes";
+  }
+  //otherwise return no
+  return "no";
  }
 }
